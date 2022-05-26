ebb218dbca010cf20a6be96d1680567932eee1f2
André Soares Balbino <asb.soares@gmail.com>
2022-05-26 03:31:58 -0300

    add php artisan comand config:cache

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -5,7 +5,7 @@ echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
 composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
-
+php artisan config:cache
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
 php artisan migrate --no-interaction --force
 
