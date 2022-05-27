ba550879f923392c1958a039c423b927b041013f
André Soares Balbino <asb.soares@gmail.com>
2022-05-26 23:09:04 -0300

    change dockerfile

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -11,4 +11,4 @@ php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 
-supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
+#supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
