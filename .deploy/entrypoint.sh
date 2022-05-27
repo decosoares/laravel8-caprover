b84ce38b6b0f397abe20ae4e9db0ca6c4deb4a7e
André Soares Balbino <asb.soares@gmail.com>
2022-05-27 00:52:39 -0300

    more change dockerfile

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -2,14 +2,13 @@
 
 echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
 
-composer install --prefer-dist --no-scripts --no-dev --no-autoloader
 composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
-php artisan config:cache
+
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
 php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 
-#supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
+supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
